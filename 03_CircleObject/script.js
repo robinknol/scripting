836f3e86527da2831c7a019826a6a2f0eb9628e7
RobinKnol <robin@knol.email>
2019-12-06 12:37:20 +0100

    circle bounce

---
+++ 03_CircleObject/script.js
@@ -0,0 +1,61 @@
+// maak een variabele voor de canvas
+const canvas = document.getElementById('canvas');
+
+// leg een 2d-context over de canvas
+const context = canvas.getContext('2d');
+
+//maak de canvas schermvullend
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+
+// hier kun je op de canvas tekenen
+
+let cirkleObject = {};
+cirkleObject.x = 200;
+cirkleObject.y = 300;
+cirkleObject.radius = 20;
+cirkleObject.sAngle = 0;
+cirkleObject.eAngle = Math.PI * 2;
+cirkleObject.color = "yellow";
+cirkleObject.velocity_x = 2;
+cirkleObject.velocity_y = 2;
+
+
+
+cirkleObject.draw = function(){
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, this.sAngle, this.eAngle);
+    context.closePath();
+    context.stroke();
+    context.fillStyle = this.color;
+    context.fill();
+
+    if(cirkleObject.x - cirkleObject.radius <= 0) {
+        cirkleObject.velocity_x = -cirkleObject.velocity_x;
+    } 
+    
+    else if(cirkleObject.y - cirkleObject.radius <= 0) {
+        cirkleObject.velocity_y = -cirkleObject.velocity_y;
+    }
+    
+    else if(cirkleObject.x + cirkleObject.radius >= window.innerWidth) {
+        cirkleObject.velocity_x = -cirkleObject.velocity_x;
+    }
+    
+    else if(cirkleObject.y + cirkleObject.radius >= window.innerHeight) {
+        cirkleObject.velocity_y = -cirkleObject.velocity_y;
+    }
+    
+}
+
+function animate(){
+    context.clearRect(0,0,width,height);
+    cirkleObject.x += cirkleObject.velocity_x;
+    cirkleObject.y += cirkleObject.velocity_y;
+    cirkleObject.draw();
+}
+
+setInterval(animate,10)
+
