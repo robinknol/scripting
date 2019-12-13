7f636edda7f3f299e56607db59d7dd86ed0013fe
RobinKnol <robin@knol.email>
2019-12-13 12:33:23 +0100

    finish

--- 03_CircleObject/script.js
+++ 03_CircleObject/script.js
@@ -58,4 +58,3 @@ function animate(){
 }
 
 setInterval(animate,10)
-
--- 04_CirkelArray/script.js
+++ 04_CirkelArray/script.js
@@ -12,53 +12,76 @@ canvas.height = height;
 
 // hier kun je op de canvas tekenen
 
-let circleObject = {};
-circleObject.x = 400;
-circleObject.y = 200;
-circleObject.radius = 40;
-circleObject.color = "purple";
-circleObject.speedX = 3;
-circleObject.speedY = 2;
+circles = []
 
-circleObject.draw = function(){
-    context.beginPath();
-    context.lineWidth = "5";
-    context.fillStyle = circleObject.color;
-    context.arc(circleObject.x, circleObject.y, circleObject.radius, 0, 2 * Math.PI);
-    context.closePath();
-    context.fill();
-    context.stroke();
+for (let i = 0; i < 10 ; i++)
+{
+    let circleObject = {};
+    circleObject.x = getRandomint(0, width);
+    circleObject.y = getRandomint(0, height);
+    circleObject.radius = 40;
+    circleObject.color = "purple";
+    circleObject.speedX = 3;
+    circleObject.speedY = 2;
+    
+    circleObject.draw = function()
+    {
+        context.beginPath();
+        context.lineWidth = "5";
+        context.fillStyle = circleObject.color;
+        context.arc(circleObject.x, circleObject.y, circleObject.radius, 0, 2 * Math.PI);
+        context.closePath();
+        context.fill();
+        context.stroke();
+    }
+    
+    circleObject.update = function()
+    {
+        circleObject.x = circleObject.x + circleObject.speedX;
+        circleObject.y = circleObject.y + circleObject.speedY;
+
+        if(circleObject.x - circleObject.radius <= 0)
+        {
+            circleObject.speedX = -circleObject.speedX;
+        } 
+        
+        else if(circleObject.y - circleObject.radius <= 0)
+        {
+            circleObject.speedY = -circleObject.speedY;
+        }
+        
+        else if(circleObject.x + circleObject.radius >= window.innerWidth)
+        {
+            circleObject.speedX = -circleObject.speedX;
+        }
+        
+        else if(circleObject.y + circleObject.radius >= window.innerHeight)
+        {
+            circleObject.speedY = -circleObject.speedY;
+        }
+    }
+
+    circles.push(circleObject);
 }
 
-circleObject.update = function(){
-    circleObject.x = circleObject.x + circleObject.speedX;
-    circleObject.y = circleObject.y + circleObject.speedY;
-
-    if(circleObject.x - circleObject.radius <= 0) {
-        circleObject.speedX = -circleObject.speedX;
-    } 
-    
-    else if(circleObject.y - circleObject.radius <= 0) {
-        circleObject.speedY = -circleObject.speedY;
-    }
-    
-    else if(circleObject.x + circleObject.radius >= window.innerWidth) {
-        circleObject.speedX = -circleObject.speedX;
-    }
-    
-    else if(circleObject.y + circleObject.radius >= window.innerHeight) {
-        circleObject.speedY = -circleObject.speedY;
-    }
-}
 
 
-
-function anime(){
+function anime()
+{
     context.clearRect(0, 0, width, height);
-    // circleObject.x += circleObject.speedX;
-    // circleObject.y += circleObject.speedY;
-    circleObject.update();
-    circleObject.draw();
+    for (let i = 0; i < circles.length; i++)
+    {
+        circles[i].update();
+        circles[i].draw();
+    }
 }
 
 setInterval(anime, 10);
+
+
+function getRandomint(min, max)
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + 1
+}
